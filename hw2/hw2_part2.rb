f58273d9b847fb5d686199b7b443761d5b277694
rilian <dmitriyis@gmail.com>
2012-03-11 14:35:51 +0200

    hw2 part2

--- hw2/hw2_part2.rb
+++ hw2/hw2_part2.rb
@@ -1,16 +1,28 @@
 class CartesianProduct
   include Enumerable
-  # your code here
+
+  def initialize(a, b)
+    @values = []
+    unless a == [] || b == []
+      a.collect {|x| b.collect {|q| @values << [x,q] } }
+    end
+  end
+
+  def each(&block)
+    @values.each(&block)
+  end
 end
 
-c = CartesianProduct.new([:a,:b], [4,5])
-c.each { |elt| puts elt.inspect }
-# [:a, 4]
-# [:a, 5]
-# [:b, 4]
-# [:b, 5]
+if ENV['TEST']
+  c = CartesianProduct.new([:a,:b], [4,5])
+  c.each { |elt| puts elt.inspect }
+  # [:a, 4]
+  # [:a, 5]
+  # [:b, 4]
+  # [:b, 5]
 
-c = CartesianProduct.new([:a,:b], [])
-c.each { |elt| puts elt.inspect }
-# (nothing printed since Cartesian product
-# of anything with an empty collection is empty)
+  c = CartesianProduct.new([:a,:b], [])
+  c.each { |elt| puts elt.inspect }
+  # (nothing printed since Cartesian product
+  # of anything with an empty collection is empty)
+end
