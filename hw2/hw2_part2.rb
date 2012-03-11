1b5b8723e66e8527eee311f513f40b0b691a3bb2
rilian <dmitriyis@gmail.com>
2012-03-11 14:06:46 +0200

    hw2 stubs

--- hw2/hw2_part2.rb
+++ hw2/hw2_part2.rb
@@ -1,1 +1,16 @@
-#stub
+class CartesianProduct
+  include Enumerable
+  # your code here
+end
+
+c = CartesianProduct.new([:a,:b], [4,5])
+c.each { |elt| puts elt.inspect }
+# [:a, 4]
+# [:a, 5]
+# [:b, 4]
+# [:b, 5]
+
+c = CartesianProduct.new([:a,:b], [])
+c.each { |elt| puts elt.inspect }
+# (nothing printed since Cartesian product
+# of anything with an empty collection is empty)
---
+++ hw2/hw2_part5.rb
@@ -0,0 +1,1 @@
+http://myapp.heroku.com/
