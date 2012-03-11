40da41b253249695da2b6c4f059e6e4c3227b8e5
rilian <dmitriyis@gmail.com>
2012-03-12 00:35:00 +0200

    extract folder

---
+++ hw2/hw2_rottenpotatoes_git/app/controllers/movies_controller.rb
@@ -0,0 +1,58 @@
+class MoviesController < ApplicationController
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    session[:params] ||= {}
+    old = session[:params]
+
+    redirect_to movies_path({:q => old[:q], :ratings => old[:ratings]}) if (params[:q].blank? && old[:q].present?) || (params[:ratings].blank? && old[:ratings].present?)
+
+    params[:q] = old[:q] if params[:q].nil?
+    params[:ratings] = old[:ratings] if params[:ratings].nil?
+
+    session[:params] = {:q => params[:q], :ratings => params[:ratings]}
+    params = session[:params]
+
+    @all_ratings = Movie::RATINGS
+
+    movie = Movie
+    movie = movie.where('rating in (?)', params[:ratings].keys) if params[:ratings]
+
+    @q = movie.search(params[:q])
+    @movies = @q.result(:distinct => true)
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+end
